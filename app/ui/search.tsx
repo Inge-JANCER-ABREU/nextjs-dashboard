b53a1ff3db82a7f449ceceda87a340f9d3251ce5
Inge_JANCER_ABreu <jansera574@gmail.com>
2024-04-02 19:15:49 -0400

    feat: add useDeouncecallbak

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,8 +1,7 @@
 'use client';
-import { useDebouncedCallback } from 'use-debounce';
-
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
@@ -23,7 +22,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
     replace(`${pathname}?${params.toString()}`);
   }, 300);
   }
-
+  
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
