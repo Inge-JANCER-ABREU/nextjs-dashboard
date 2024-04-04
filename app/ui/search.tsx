03f4cfc7c78918867b61563e2ab6669eb3121e75
Inge_JANCER_ABreu <jansera574@gmail.com>
2024-04-03 20:59:22 -0400

    delete unnecesary key

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -7,7 +7,6 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
-{
     const handleSearch = useDebouncedCallback((term) => {
     console.log(`Searching... ${term}`);
    
